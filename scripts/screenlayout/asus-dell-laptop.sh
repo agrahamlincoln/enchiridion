af51d075faaec2ad43198bb7e07dc3554d9c531c
Graham Rounds <graham@platform9.com>
2019-06-12 10:16:35 -0700

    add scripts for polybar and i3

---
+++ scripts/screenlayout/asus-dell-laptop.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 3200x1800 --pos 800x2560 --rotate normal --output DP-1-2 --mode 2560x1440 --pos 2560x0 --rotate left --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-1-1 --mode 2560x1440 --pos 0x776 --rotate normal
