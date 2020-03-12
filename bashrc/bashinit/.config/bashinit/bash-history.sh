67c950d8923459c8365ae3e8147753f323a1c873
Graham Rounds <gtrounds@gmail.com>
2020-03-12 15:32:46 -0700

    reload history on every command
    when the history is being written from different shells (like with tmux)
    the history is lost in each individual tmux session. by reloading the
    history on every command, the history is preserved between tmux sessions

--- bashrc/bashinit/.config/bashinit/bash-history.sh
+++ bashrc/bashinit/.config/bashinit/bash-history.sh
@@ -5,15 +5,18 @@
 export HISTFILESIZE=
 export HISTSIZE=
 export HISTTIMEFORMAT="[%F %T] "
-# Change the file location because certain bash sessions truncate .bash_history file upon close.
+# Change the file location because certain bash sessions truncate .bash_history
+# file upon close.
 # http://superuser.com/questions/575479/bash-history-truncated-to-500-lines-on-each-login
 export HISTFILE=~/.bash_eternal_history
 
 # Force prompt to write history after every command.
-# http://superuser.com/questions/20900/bash-history-loss
-PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
+# also reload the history after every command to sync if/when multiple shells
+# are in use. http://superuser.com/questions/20900/bash-history-loss
+PROMPT_COMMAND="history -a; history -n; $PROMPT_COMMAND"
 shopt -s histappend
 stophistory () {
   PROMPT_COMMAND="bash_prompt_command"
   echo 'History recording stopped. Make sure to `kill -9 $$` at the end of the session.'
 }
+
