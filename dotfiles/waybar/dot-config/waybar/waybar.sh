e988ade4ae006c72476a3289682c4be95fe09e00
Graham Rounds <gtrounds@gmail.com>
2025-03-08 12:36:05 -0700

    feat: color theme + live reload of waybar on config changes

--- dotfiles/waybar/dot-config/waybar/waybar.sh
+++ dotfiles/waybar/dot-config/waybar/waybar.sh
@@ -6,6 +6,23 @@ killall -q waybar
 # Wait until processes have shut down
 while pgrep -x waybar >/dev/null; do sleep 1; done
 
-#Launch main
-waybar
+CONFIG_FILES="$HOME/.config/waybar/config.jsonc $HOME/.config/waybar/style.css"
 
+# Check if inotifywait is available
+if command -v inotifywait >/dev/null 2>&1; then
+  USE_INOTIFY=true
+else
+  USE_INOTIFY=false
+  echo "Warning: inotify-tools not found. Automatic config reloading disabled. Install inotify-tools for this functionality." >&2
+fi
+
+# Launch Waybar based on inotify availability
+if [ "$USE_INOTIFY" = true ]; then
+    while true; do
+        waybar &
+        inotifywait -e create,modify $CONFIG_FILES
+        killall waybar
+    done
+else
+    waybar
+fi
